48b985416b93600271e6587d94405e2b1cc31ee5
S-Lombardi <silo.lombardi@gmail.com>
2023-11-03 19:54:35 +0100

    Rinominato fine welcome in home.php - corretta anche la rotta in web.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,6 +36,8 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+
+        //SALVATAGGIO USER
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
