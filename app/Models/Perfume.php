2c54089a17b59113c84b7cd1dfacf00d7a5e1c87
S-Lombardi <silo.lombardi@gmail.com>
2023-11-06 16:08:57 +0100

    compilato fillable del Model Perfume

--- app/Models/Perfume.php
+++ app/Models/Perfume.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Users;
 
 class Perfume extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'volume_ml',
+        'fragrance_family',
+        'top_notes',
+        'heart_notes',
+        'base_notes',
+        'price',
+        'quantity_evailable',
+        'user_id',
+
+    ]
 }
