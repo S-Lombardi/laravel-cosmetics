e7ea6413a71b5c29e8130214f68bf1d91ae8ed35
S-Lombardi <silo.lombardi@gmail.com>
2023-11-06 13:09:54 +0100

    Creazione tabella perfume con le varie colonne

--- database/migrations/2023_11_06_094302_create_perfumes_table.php
+++ database/migrations/2023_11_06_094302_create_perfumes_table.php
@@ -16,6 +16,15 @@ return new class extends Migration
         Schema::create('perfumes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('volume_ml'); //solo numeri interi non negativi
+            $table->string('fragrance_family');
+            $table->string('top_notes'); //note di testa
+            $table->string('heart_notes'); //note di cuore
+            $table->string('base_notes'); //note di fondo 
+            $table->float('price', 5, 2);
+            $table->unsignedInteger('quantity_evailable');
         });
     }
 
